022ea082006bcfb66965e9d648a79835c8f1c1ec
devdiagon <fredericktimo@gmail.com>
2025-09-12 21:34:17 -0500

    initial server running

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,1 +1,26 @@
 package api
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct {
+	address string
+	db      *sql.DB
+}
+
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
+	return &APIServer{
+		address: addr,
+		db:      db,
+	}
+}
+
+func (s *APIServer) Run() error {
+	router := mux.NewRouter()
+
+	return http.ListenAndServe(s.address, router)
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"github.com/devdiagon/gomerce/cmd/api"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	server := api.NewAPIServer(":8000", nil)
+	err := server.Run()
+
+	if err != nil {
+		log.Error(err)
+	}
+}
