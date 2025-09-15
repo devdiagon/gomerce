7d7ece1b9a9e2df28dcd746945bd9dba33cc8982
devdiagon <fredericktimo@gmail.com>
2025-09-14 21:58:33 -0500

    created user repository and type

---
+++ service/user/store.go
@@ -0,0 +1,59 @@
+package user
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/devdiagon/gomerce/types"
+)
+
+// This is a user repository that later will be injected
+type Store struct {
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{db: db}
+}
+
+func (s *Store) GetUserByEmail(email string) (*types.User, error) {
+	rows, err := s.db.Query("SELECT * FROM users WHERE email = ?", email)
+	if err != nil {
+		return nil, err
+	}
+
+	user := new(types.User)
+
+	for rows.Next() {
+		user, err = scanRowIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	//User not Found
+	if user.ID == 0 {
+		return nil, fmt.Errorf("user not found")
+	}
+
+	return user, nil
+}
+
+func scanRowIntoUser(rows *sql.Rows) (*types.User, error) {
+	user := new(types.User)
+
+	err := rows.Scan(
+		&user.ID,
+		&user.FirstName,
+		&user.LastName,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
