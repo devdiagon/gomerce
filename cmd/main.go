5770e28d1a3ae2d45bd591119aeb41111ffc64ac
devdiagon <fredericktimo@gmail.com>
2025-09-14 21:04:59 -0500

    db conn variables

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"github.com/devdiagon/gomerce/cmd/api"
+	"github.com/devdiagon/gomerce/config"
 	"github.com/devdiagon/gomerce/db"
+	"github.com/go-sql-driver/mysql"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	db, err := db.NewMySQLStorage()
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBADDRESS,
+		DBName:               config.Envs.DBNAME,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	server := api.NewAPIServer(":8000", nil)
 	err := server.Run()
---
+++ config/env.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	PublicHost string
+	Port       string
+	DBUser     string
+	DBPassword string
+	DBADDRESS  string
+	DBNAME     string
+}
+
+var Envs = initConfig()
+
+func getEnv(key, fallback string) string {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+
+	return value
+}
+
+func initConfig() Config {
+	return Config{
+		PublicHost: getEnv("PUBLIC_HOST", "http://localhost"),
+		Port:       getEnv("PORT", "8080"),
+		DBUser:     getEnv("DB_USER", "admin"),
+		DBPassword: getEnv("DB_PASSWORD", "admin"),
+		DBADDRESS:  fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
+		DBNAME:     getEnv("DB_NAME", "gomerce"),
+	}
+}
