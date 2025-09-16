3a0d14717dc54f9267cc655c36d7822b90331616
devdiagon <fredericktimo@gmail.com>
2025-09-15 20:57:19 -0500

    initial migration setup

---
+++ cmd/migrate/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"os"
+
+	"github.com/devdiagon/gomerce/config"
+	"github.com/devdiagon/gomerce/db"
+	mysqlCfg "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	db, err := db.NewMySQLStorage(mysqlCfg.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBADDRESS,
+		DBName:               config.Envs.DBNAME,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	driver, err := mysql.WithInstance(db, &mysql.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://cmd/migrate/migrations",
+		"mysql",
+		driver,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Get command from the CLI
+	cmd := os.Args[(len(os.Args) - 1)]
+	if cmd == "up" {
+		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	}
+
+	if cmd == "down" {
+		if err := m.Down(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	}
+}
